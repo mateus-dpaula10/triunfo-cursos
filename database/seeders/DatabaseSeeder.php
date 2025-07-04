a9b63d690c10a8bbea2ff75767a3f0f302ae039d
Mateus De Paula <74686833+mateus-dpaula10@users.noreply.github.com>
2025-07-04 09:54:53 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,14 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
-            'name' => 'Fabrício Manfré',
-            'password' => bcrypt('19021985'),
-            'cpf' => '32642701883',
-            'birth_date' => '1985-02-19',
-            'phone' => '11937533715',
-            'role' => 'admin',
-            'description' => 'Usuário administrador do sistema'
-        ]);
+        
     }
 }
